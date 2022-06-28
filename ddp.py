f75c4224dd21f454f62e1bf7ee607d6fa9e122b4
Ahmet Hakan Duman <ahmet.hakan.duman@rwth-aachen.de>
2022-06-28 10:50:01 +0200

    replaced i with t as timestep indexer. added logger to debug algorithm.

--- ddp.py
+++ ddp.py
@@ -1,8 +1,8 @@
 from functools import partial
-from jax import grad,jacrev, jit, lax, jacfwd
+from jax import grad, jacrev, jit, lax, jacfwd
 import jax.numpy as jnp
 from time import time
-import warnings
+import logging
 from pygame import K_s
 from tabulate import tabulate
 import numpy as np
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import itertools
 
 plt.ion()
+logging.basicConfig(level=logging.WARNING)
 
 
 class DDP:
@@ -35,8 +36,8 @@ class DDP:
         self.l_xx = jit(jacfwd(self.l_x, 0))
         self.l_uu = jit(jacfwd(self.l_u, 1))
         self.l_ux = jit(jacfwd(self.l_u, 0))
-        self.f_x = jit(jacrev(self.f, 0))
-        self.f_u = jit(jacrev(self.f, 1))
+        self.f_x = jit(jacfwd(self.f, 0))
+        self.f_u = jit(jacfwd(self.f, 1))
         self.f_xx = jit(jacfwd(self.f_x, 0))
         self.f_uu = jit(jacfwd(self.f_u, 1))
         self.f_ux = jit(jacfwd(self.f_u, 0))
@@ -62,15 +63,15 @@ class DDP:
         # plot initialization
         self.fig, self.axs = plt.subplots(2, 2)
         self.axs = self.axs.flatten()
-        self.plt = list(itertools.chain(*[ax.plot([],[]) for ax in self.axs]))
+        self.plt = list(itertools.chain(*[ax.plot([], []) for ax in self.axs]))
 
     @partial(jit, static_argnums=(0,))
     def is_pos_def(self, x):
         return jnp.all(jnp.diagonal(jnp.linalg.cholesky(x)) > 0)
 
     @partial(jit, static_argnums=(0,))
-    def backward(self, x_seq, u_seq, lmbda):
-        
+    def backward_pass(self, x_seq, u_seq, lmbda):
+
         """
         After the observation of high computation time with classic python loop.
         Backward-pass is implemented using jax's while loop, where the condition is checking whether non-positive def. q_uu.
@@ -90,88 +91,88 @@ class DDP:
         kk_seq = jnp.array(jnp.zeros((self.N - 1, self.n)))
         dV = jnp.array([0.0, 0.0])
 
-        def loop_cond(carry):
+        def backward_loop_cond(carry):
             # these needs to be carried for jax to track
-            t, x, u, v_x, v_xx, v_xx_reg, _, _, _ = carry
-            f_u_t = self.f_u(x[t], u[t])
-            v_xx_reg = v_xx_reg.at[t + 1].set(
-                v_xx[t + 1] + lmbda * jnp.eye(self.n) * (self.regType == 1)
+            i, x, u, v_x, v_xx, v_xx_reg, _, _, _ = carry
+            f_u_i = self.f_u(x[i], u[i])
+            v_xx_reg = v_xx_reg.at[i + 1].set(
+                v_xx[i + 1] + lmbda * jnp.eye(self.n) * (self.regType == 1)
             )
-            tmp_reg = f_u_t.T @ v_xx_reg[t + 1]
+            tmp_reg = f_u_i.T @ v_xx_reg[i + 1]
 
             q_uu_reg = (
-                self.l_uu(x[t], u[t])
-                + tmp_reg @ f_u_t
+                self.l_uu(x[i], u[i])
+                + tmp_reg @ f_u_i
                 + lmbda * (self.regType == 2)
-                + jnp.dot(v_x[t + 1], jnp.squeeze(self.f_uu(x[t], u[t])))
+                + jnp.dot(v_x[i + 1], jnp.squeeze(self.f_uu(x[i], u[i])))
             )
 
-            return self.is_pos_def(q_uu_reg) & (t >= 0)
+            return self.is_pos_def(q_uu_reg) & (i >= 0)
 
-        def backward_loop(carry):
+        def backward_loop_body(carry):
             # these needs to be carried for jax to track
-            t, x, u, v_x, v_xx, v_xx_reg, k, kk, dv = carry
-            f_x_t = self.f_x(x[t], u[t])
-            f_u_t = self.f_u(x[t], u[t])
-            q_x = self.l_x(x[t], u[t]) + f_x_t.T @ v_x[t + 1]
-            q_u = self.l_u(x[t], u[t]) + f_u_t.T @ v_x[t + 1]
+            i, x, u, v_x, v_xx, v_xx_reg, k, kk, dv = carry
+            f_x_i = self.f_x(x[i], u[i])
+            f_u_i = self.f_u(x[i], u[i])
+            q_x = self.l_x(x[i], u[i]) + f_x_i.T @ v_x[i + 1]
+            q_u = self.l_u(x[i], u[i]) + f_u_i.T @ v_x[i + 1]
             q_xx = (
-                self.l_xx(x[t], u[t])
-                + (f_x_t.T @ v_xx[t + 1]) @ f_x_t
-                + jnp.dot(v_x[t + 1], jnp.squeeze(self.f_xx(x[t], u[t])))
+                self.l_xx(x[i], u[i])
+                + (f_x_i.T @ v_xx[i + 1]) @ f_x_i
+                + jnp.dot(v_x[i + 1], jnp.squeeze(self.f_xx(x[i], u[i])))
             )
 
-            tmp = f_u_t.T @ v_xx[t + 1]
+            tmp = f_u_i.T @ v_xx[i + 1]
 
             q_ux = (
-                self.l_ux(x[t], u[t])
-                + tmp @ f_x_t
-                + jnp.dot(v_x[t + 1], jnp.squeeze(self.f_ux(x[t], u[t])))
+                self.l_ux(x[i], u[i])
+                + tmp @ f_x_i
+                + jnp.dot(v_x[i + 1], jnp.squeeze(self.f_ux(x[i], u[i])))
             )
 
             q_uu = (
-                self.l_uu(x[t], u[t])
-                + tmp @ f_u_t
-                + jnp.dot(v_x[t + 1], jnp.squeeze(self.f_uu(x[t], u[t])))
+                self.l_uu(x[i], u[i])
+                + tmp @ f_u_i
+                + jnp.dot(v_x[i + 1], jnp.squeeze(self.f_uu(x[i], u[i])))
             )
 
-            v_xx_reg = v_xx_reg.at[t + 1].set(
-                v_xx[t + 1] + lmbda * jnp.eye(self.n) * (self.regType == 1)
+            v_xx_reg = v_xx_reg.at[i + 1].set(
+                v_xx[i + 1] + lmbda * jnp.eye(self.n) * (self.regType == 1)
             )
-            tmp_reg = f_u_t.T @ v_xx_reg[t + 1]
+            tmp_reg = f_u_i.T @ v_xx_reg[i + 1]
 
             q_ux_reg = (
-                self.l_ux(x[t], u[t])
-                + tmp_reg @ f_x_t
-                + jnp.dot(v_x[t + 1], jnp.squeeze(self.f_ux(x[t], u[t])))
+                self.l_ux(x[i], u[i])
+                + tmp_reg @ f_x_i
+                + jnp.dot(v_x[i + 1], jnp.squeeze(self.f_ux(x[i], u[i])))
             )
 
             q_uu_reg = (
-                self.l_uu(x[t], u[t])
-                + tmp_reg @ f_u_t
+                self.l_uu(x[i], u[i])
+                + tmp_reg @ f_u_i
                 + lmbda * (self.regType == 2)
-                + jnp.dot(v_x[t + 1], jnp.squeeze(self.f_uu(x[t], u[t])))
+                + jnp.dot(v_x[i + 1], jnp.squeeze(self.f_uu(x[i], u[i])))
             )
             inv_q_uu_reg = jnp.linalg.inv(q_uu_reg)
-            k_t = -inv_q_uu_reg @ q_u
-            kk_t = -inv_q_uu_reg @ q_ux_reg
+            k_i = -inv_q_uu_reg @ q_u
+            kk_i = -inv_q_uu_reg @ q_ux_reg
 
-            dv += jnp.array([k_t.T @ q_u, 0.5 * (k_t.T @ q_uu) @ k_t])
+            dv += jnp.array([k_i.T @ q_u, 0.5 * (k_i.T @ q_uu) @ k_i])
 
-            kk_t_q_uu = kk_t.T @ q_uu
-            v_x = v_x.at[t].set(q_x + kk_t_q_uu @ k_t + kk_t.T @ q_u + q_ux.T @ k_t)
-            v_xx = v_xx.at[t].set(
-                q_xx + kk_t_q_uu @ kk_t + kk_t.T @ q_ux + q_ux.T @ kk_t
+            kk_i_q_uu = kk_i.T @ q_uu
+            v_x = v_x.at[i].set(q_x + kk_i_q_uu @ k_i + kk_i.T @ q_u + q_ux.T @ k_i)
+            v_xx = v_xx.at[i].set(
+                q_xx + kk_i_q_uu @ kk_i + kk_i.T @ q_ux + q_ux.T @ kk_i
             )
-            v_xx = v_xx.at[t].set(0.5 * (v_xx[t].T + v_xx[t]))
-            k = k.at[t].set(k_t)
-            kk = kk.at[t].set(kk_t.T[0])
+            v_xx = v_xx.at[i].set(0.5 * (v_xx[i].T + v_xx[i]))
+            k = k.at[i].set(k_i)
+            kk = kk.at[i].set(kk_i.T[0])
 
-            return t - 1, x, u, v_x, v_xx, v_xx_reg, k, kk, dv
+            return i - 1, x, u, v_x, v_xx, v_xx_reg, k, kk, dv
 
         seqs_all = lax.while_loop(
-            loop_cond,
-            backward_loop,
+            backward_loop_cond,
+            backward_loop_body,
             init_val=(self.N - 1, x_seq, u_seq, V_x, V_xx, V_xx_reg, k_seq, kk_seq, dV),
         )
 
@@ -186,20 +187,20 @@ class DDP:
         return V_x, V_xx, k_seq, kk_seq, dv, diverge
 
     @partial(jit, static_argnums=(0,))
-    def forward(self, x_seq, u_seq, k_seq, kk_seq, alpha):
+    def forward_pass(self, x_seq, u_seq, k_seq, kk_seq, alpha):
         x_seq_hat = jnp.array(jnp.zeros(x_seq.shape))
         x_seq_hat = x_seq_hat.at[0].set(self.x0)
         u_seq_hat = jnp.array(jnp.zeros(u_seq.shape))
 
-        def f_loop(t, seqs):
+        def forward_loop(i, seqs):
             x_hat, u_hat, x, u, k, kk = seqs
-            control = alpha * k[t] + kk[t] @ (x_hat[t] - x[t])
-            u_hat = u_hat.at[t].set(u[t] + control[0])
-            x_hat = x_hat.at[t + 1].set(self.f(x_hat[t], u_hat[t]))
+            control = alpha * k[i] + kk[i] @ (x_hat[i] - x[i])
+            u_hat = u_hat.at[i].set(u[i] + control[0])
+            x_hat = x_hat.at[i + 1].set(self.f(x_hat[i], u_hat[i]))
             return x_hat, u_hat, x, u, k, kk
 
         seqs_all = lax.fori_loop(
-            0, self.N - 1, f_loop, (x_seq_hat, u_seq_hat, x_seq, u_seq, k_seq, kk_seq)
+            0, self.N - 1, forward_loop, (x_seq_hat, u_seq_hat, x_seq, u_seq, k_seq, kk_seq)
         )
         return seqs_all[0], seqs_all[1]  # 0: x_seq_hat, 1: u_seq_hat
 
@@ -211,17 +212,17 @@ class DDP:
         k_seq = jnp.array(jnp.zeros((self.N - 1, self.m)))
         kk_seq = jnp.array(jnp.zeros((self.N - 1, self.n)))
 
-        x_seq, _ = self.forward(x_seq, u_seq, k_seq, kk_seq, 1)
+        x_seq, _ = self.forward_pass(x_seq, u_seq, k_seq, kk_seq, 1)
 
         cost = self.l(x_seq, u_seq) + self.lf(x_seq[-1])  # initial cost
-        dcost = 0.0
+        dcost = 0.0  # initialize reduction in cost
 
         lmbda = self.lmbda
         dlambda = self.dlambda
         lambdaFactor = self.lambdaFactor
         lambdaMin = self.lambdaMin
         lambdaMax = self.lambdaMax
-        Alpha = 0.8 ** (jnp.linspace(0, 100, 101))
+        Alpha = 0.8 ** (jnp.linspace(0, 50, 51))
 
         for i in range(self.maxIter):
 
@@ -229,7 +230,7 @@ class DDP:
             backward_start = time()
 
             while not back_pass_done:
-                V_x, V_xx, k_seq, kk_seq, dv, diverge = self.backward(
+                V_x, V_xx, k_seq, kk_seq, dv, diverge = self.backward_pass(
                     x_seq, u_seq, lmbda
                 )
 
@@ -250,7 +251,7 @@ class DDP:
             if back_pass_done:
                 fwd_start = time()
                 for alpha in Alpha:
-                    x_new, u_new = self.forward(x_seq, u_seq, k_seq, kk_seq, alpha)
+                    x_new, u_new = self.forward_pass(x_seq, u_seq, k_seq, kk_seq, alpha)
                     running_cost_new, final_cost_new = self.l(x_new, u_new), self.lf(
                         x_new[-1]
                     )
@@ -261,20 +262,22 @@ class DDP:
                         z = dcost / expected
                     else:
                         z = jnp.sign(dcost)
-                        warnings.warn(
+                        logging.warn(
                             "Non-positive expected reduction of cost: Should not occur!"
                         )
                     if z > 0:
                         fwd_pass_done = True
                         break
-                    
+                    else:
+                        logging.warn("Current alpha: %.5f failed in reducing cost. -------> Decreasing alpha...",alpha)
+
                 # if not fwd_pass_done:
                 #     warnings.warn("Failed to find an alpha to decrease the cost!")
 
                 fwd_finish = time() - fwd_start
 
             if fwd_pass_done:
-
+                logging.warn("Forward-pass succeed. --------> Decreasing lambda.")
                 # decrease lambda
                 dlambda = min(dlambda / lambdaFactor, 1 / lambdaFactor)
                 lmbda = lmbda * dlambda * (lmbda > lambdaMin)
@@ -318,22 +321,31 @@ class DDP:
                     break
 
             else:  # no cost improvement
+                logging.warn("No cost improvement, inceasing lambda")
                 # increase lambda
                 dlambda = max(dlambda * lambdaFactor, lambdaFactor)
                 lmbda = max(lmbda * dlambda, lambdaMin)
 
                 # terminate ?
                 if lmbda > lambdaMax:
+                    logging.warn("lambda reached to it's setted maximum.")
                     break
+            if i == self.maxIter:
+                logging.warn("Maximum number of Iteration: %d is done ",i)
 
         return x_seq, u_seq
 
     def graphics(self, i, V, z, lmbda, alpha):
 
-        yy = [V,lmbda,alpha,z]
-        titles = ["Cost", "Lambda (Regularization Param.)", "Alpha (Line-Search Param.)", "Reduction/Estimated"]
+        yy = [V, lmbda, alpha, z]
+        titles = [
+            "Cost",
+            "Lambda (Regularization Param.)",
+            "Alpha (Line-Search Param.)",
+            "Reduction/Estimated",
+        ]
         colors = ["b", "r", "g", "c"]
-        
+
         for k, y in enumerate(yy):
             self.plt[k].set_xdata(jnp.append(self.plt[k].get_xdata(), i))
             self.plt[k].set_ydata(jnp.append(self.plt[k].get_ydata(), yy[k]))
@@ -341,9 +353,9 @@ class DDP:
             self.axs[k].relim()
             self.axs[k].autoscale_view()
             self.axs[k].set_title(titles[k])
-            
+
             if y == V:
-                self.axs[0].set_yscale('log')
+                self.axs[0].set_yscale("log")
 
             self.fig.canvas.draw()
             self.fig.canvas.flush_events()
